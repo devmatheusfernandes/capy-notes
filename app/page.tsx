3f906af7a7b6ba900ae79be7cde8d464304b30d9
devmatheusfernandes <dev.matheusfernandes@gmail.com>
2025-12-10 23:19:06 -0300

    Theme and adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { ModeToggle } from "@/components/ui/mode-toggle";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <div className="flex items-center justify-center h-screen dark:bg-gray-800">
-        <button className="px-4 py-2 border flex gap-2 border-slate-200 dark:border-slate-700 rounded-lg text-slate-700 dark:text-slate-200 hover:border-slate-400 dark:hover:border-slate-500 hover:text-slate-900 dark:hover:text-slate-300 hover:shadow transition-all ease-in-out duration-200">
+      <div className="relative flex items-center justify-center h-screen dark:bg-gray-800">
+        <div className="absolute top-4 right-4">
+          <ModeToggle />
+        </div>
+
+        <Button>
           <Image width={24} height={24} className="w-6 h-6" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo" />
           <span>Login with Google</span>
-        </button>
+        </Button>
+        
       </div>
     </div>
   );
